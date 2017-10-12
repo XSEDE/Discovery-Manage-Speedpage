4c14be4ff495da8adda570209ae4444da63a11e4
navarro <navarro@1370f987-b873-4e1f-8c66-8b3e5403fd71>
2017-10-12 16:05:11 +0000

    Minor fixes

--- xci/source/info.apps/Manage-Speedpage/trunk/sbin/route_speedpage.sh
+++ xci/source/info.apps/Manage-Speedpage/trunk/sbin/route_speedpage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PYTHON_ROOT=/soft/python-current
-export PYTHON_EXEC=${PYTHON_ROOT}/bin/python
-export LD_LIBRARY_PATH=${PYTHON_ROOT}/lib
-export PYTHONPATH=/soft/warehouse-1.0/PROD/django_xsede_warehouse
+MY_ROOT=/soft/warehouse-apps-1.0/Manage-Speedpage/PROD
+WAREHOUSE_ROOT=/soft/warehouse-1.0/PROD
+PYTHON=/soft/python-current/bin/python
+export LD_LIBRARY_PATH=/soft/python-current/lib/
+
+export PYTHONPATH=${WAREHOUSE_ROOT}/django_xsede_warehouse
 export DJANGO_CONF=/soft/warehouse-1.0/conf/settings_info_mgmt.conf
 export DJANGO_SETTINGS_MODULE=xsede_warehouse.settings
-#python ./serialize_save.py
-#python ./route_speedpage.py ${@:1}
-#python ./speedserialize ${@:1}
-${PYTHON_EXEC} /soft/warehouse-apps-1.0/Manage-Speedpage/PROD/sbin/route_speedpage.py ${@:1}
+
+${PYTHON} ${MY_ROOT}/sbin/route_speedpage.py ${@:1}

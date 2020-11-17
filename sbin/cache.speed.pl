c3d28014bf61fd3578555360aabf64b14043f549
JP Navarro <jpfnavarro@gmail.com>
2020-11-17 20:45:18 +0000

    Prepapre for GIT

--- sbin/cache.speed.pl
+++ sbin/cache.speed.pl
@@ -14,11 +14,12 @@ my $DBNAME = 'speedpage';
 #my $DBUSER = 'guest';
 #my $DBPASS = 'tggridinfo'; 
 my $DBUSER = 'xsede';
-my $DBPASS = 'xsedegridinfo'; 
+my $DBPASS = '<SPEEDPASS>'; 
 my $DBHOST = 'quipu.psc.xsede.org'; 
 my $DBPORT = '3306'; 
 
-my $mapCSV = '/soft/warehouse-apps-1.0/Manage-XDCDB/var/tgresources.csv';
+#my $mapCSV = '/soft/warehouse-apps-1.0/Manage-XDCDB/var/tgresources.csv';
+my $mapCSV = '/soft/warehouse-apps-1.0/Manage-XDCDB-Resources/var/tgresources.csv';
 open DB,"<$mapCSV" or
   die "Failed to open $mapCSV";
 my %name_to_id;
